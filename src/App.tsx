0cf76ccf92b87ef9b6fee56739924a17623da565
Konrad <dzumanet@gmail.com>
2024-12-30 06:13:32 +0100

    feat: configure TanStack Router with QueryClient and context integration. feat: create main route path

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen.ts";
+
+const router = createRouter({
+    routeTree,
+    context: {
+        queryClient: undefined!,
+    }
+});
+
+
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router;
+    }
+}
+
+const queryClient = new QueryClient;
+
 export const App = () => {
-    return null;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} context={{ queryClient }} />
+        </QueryClientProvider>
+    );
 };
