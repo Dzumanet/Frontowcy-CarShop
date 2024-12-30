babfebddacd8d04fb0f3c3127bded86e782558a1
Konrad <dzumanet@gmail.com>
2024-12-30 07:44:43 +0100

    feat: create types for the project

---
+++ src/types/index.ts
@@ -0,0 +1,34 @@
+export type Category = {
+    id: string;
+    name: string;
+    identifier: string;
+    position: number;
+}
+
+export type CategoryDTO = Omit<Category, 'id'>;
+
+export type CategoryWithParts =Category & {
+    parts: Part[];
+
+}
+
+export type Part = {
+    id: string;
+    name: string;
+    price: number;
+    partId: string;
+    categoryId: string;
+}
+
+export type PartDTO = Omit<Part, 'id'>;
+
+export type Order = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    value: number;
+    details: string;
+}
+
+export type OrderDTO = Omit<Order, 'id'>;
