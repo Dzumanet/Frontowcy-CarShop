0f88b7f049cf31f6ecdadc2fbd8eaa643e57e215
Konrad <dzumanet@gmail.com>
2025-01-13 12:59:23 +0100

    feat: enhance delete category mutation to remove associated parts.

--- src/mutation/useDeleteCategoryMutation.ts
+++ src/mutation/useDeleteCategoryMutation.ts
@@ -1,17 +1,27 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { apiCall } from "../utils/apiCall.ts";
-import { Category } from "../types";
+import { Category, Part } from "../types";
 
 export const useDeleteCategoryMutation = () => {
     const queryClient = useQueryClient();
 
     return useMutation({
         mutationKey: ['delete-category'],
-        mutationFn: async(id: string) => apiCall<Category>(`categories/${id}`, {
-            method: 'DELETE',
-        }),
+        mutationFn: async (id: string) => {
+            const parts = await apiCall<Part[]>(`parts?categoryId=${id}`, { method: 'GET' });
+
+            if (parts.length > 0) {
+                for (const part of parts) {
+                    await apiCall<Part>(`parts/${part.id}`, { method: 'DELETE' });
+                }
+            }
+
+            return apiCall<Category>(`categories/${id}`, { method: 'DELETE' });
+
+        },
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['category'] });
-        }
-    })
+            queryClient.invalidateQueries({ queryKey: ['parts'] });
+        },
+    });
 };
